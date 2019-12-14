c653f52c78c1079fdebe1e683601d6a8eda48cc4
Greenhoody <1mk2mk3mk@gmail.com>
2019-12-14 11:23:03 +0100

    Time to work

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 public class Main {
     public static void main(String[] args) {
         try {
-            URL url = new URL("tesla.iem.pw.edu.pl:4444/f95a1461/1/possibilities");
+            URL url = new URL("http://tesla.iem.pw.edu.pl:4444/f95a1461/1/possibilities");
             HttpClient con = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("tesla.iem.pw.edu.pl:4444/f95a1461/1/possibilities")).GET().build();
-            HttpResponse response = con.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://tesla.iem.pw.edu.pl:4444/f95a1461/1/possibilities")).build();
+            HttpResponse<String> response = con.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.printf("%s \n",response.statusCode());
+            System.out.printf(response.body());
         } catch (UnknownHostException u) {
             System.out.println(u);
         } catch (IOException i) {
@@ -21,6 +21,7 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
 
     }
 }

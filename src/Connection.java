d0a66d03843d9f4ececf241d9e31b528e6f45589
Greenhoody <1mk2mk3mk@gmail.com>
2019-12-18 14:24:50 +0100

    Comunication with server.

--- src/Connection.java
+++ src/Connection.java
@@ -11,18 +11,30 @@ import java.util.regex.Pattern;
 public class Connection {
     static String[] patterns = {"up\\\"\\:\\\"\\+", "right\\\"\\:\\\"\\+", "down\\\"\\:\\\"\\+", "left\\\"\\:\\\"\\+"};
 
+    public static void move (int numberOfLabyrinth, String UID, String direction){
+        if (direction != "right" && direction != "left" && direction != "up" && direction != "down"){
+            System.err.print("Incorrect way");
+            return;
+        }
+        //TODO
+        StringBuilder addressCreator = new StringBuilder();
+        String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/move/").append(direction).toString();
+        HttpClient con = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
+    }
+
+    public static void uploadMap (char[][] map){
+        //TODO
+    }
+
     public static char[] getPossibilities(int numberOfLabyrinth, String UID) {
         char[] anserw = new char[4];
         try {
             //Creating address from data passed by user
             StringBuilder addressCreator = new StringBuilder();
             String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/possibilities").toString();
-            //Making request to server
-            HttpClient con = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
-            HttpResponse<String> response = con.send(request, HttpResponse.BodyHandlers.ofString());
             //Making response from server into return values
-            String body = response.body().toString();
+            String body = getBodyResponse(address);
             System.out.printf(body, "\n");
             for (int i = 0; i < 4; i++) {
                 Pattern compilePattern = Pattern.compile(patterns[i]);
@@ -48,21 +60,58 @@ public class Connection {
             //Creating address from data passed by user
             StringBuilder addressCreator = new StringBuilder();
             String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/size").toString();
-            //Making request to server
-            HttpClient con = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
-            HttpResponse<String> response = con.send(request, HttpResponse.BodyHandlers.ofString());
             //Making response from server into return values
-            String body = response.body();
+            String body = getBodyResponse(address);
             System.out.printf(body, "\n");
             int tmp = body.indexOf("x");
-            size[0] = Integer.parseInt(body.substring(0,(tmp)));
-            size[1] = Integer.parseInt(body.substring(tmp+1));
+            size[0] = Integer.parseInt(body.substring(0, (tmp)));
+            size[1] = Integer.parseInt(body.substring(tmp + 1));
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return size;
+    }
+
+    public static int[] getPosition(int numberOfLabyrinth, String UID) {
+        int[] position = new int[2];
+        try {
+            //Creating address from data passed by user
+            StringBuilder addressCreator = new StringBuilder();
+            String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/startposition").toString();
+            //Making response from server into return values
+            String body = getBodyResponse(address);
+            System.out.printf(body, "\n");
+            int tmp = body.indexOf("x");
+            position[0] = Integer.parseInt(body.substring(0, (tmp)));
+            position[1] = Integer.parseInt(body.substring(tmp + 1));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return position;
+    }
+
+    public static void reset(int numberOfLabyrinth, String UID) throws IOException, InterruptedException {
+        StringBuilder addressCreator = new StringBuilder();
+        String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/reset").toString();
+        //Making response from server into return values
+        String body = getBodyResponse(address);
+    }
+
+    public static int numberOfMoves(int numberOfLabyrinth, String UID) throws IOException, InterruptedException {
+        StringBuilder addressCreator = new StringBuilder();
+        String address = addressCreator.append("http://tesla.iem.pw.edu.pl:4444/").append(UID).append('/').append(numberOfLabyrinth).append("/reset").toString();
+        return Integer.parseInt(getBodyResponse(address));
+    }
+
+    private static String getBodyResponse(String address) throws IOException, InterruptedException {
+        //Making request to server
+        HttpClient con = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
+        HttpResponse<String> response = con.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
     }
 }

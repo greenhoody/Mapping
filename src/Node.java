24b8a258126e4e9312c2842544e5751b347bff0e
Rafał <duran.rafal@gmail.com>
2020-01-03 12:54:53 +0100

    New classes

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+
+public class Node {
+
+    public boolean visited;
+    public Node visitedFrom;
+    public int index;
+    public int x;
+    public int y;
+    public ArrayList<Node> neighbours;
+
+    public int findNeighbour(Node n){
+        for (int i = 0; i < neighbours.size(); i++){
+            if (neighbours.get(i) == n)
+                return i;
+        }
+        throw new IllegalArgumentException("Nie ma takiego sąsiada");
+    }
+}
+
